ba1a8ef579292412e6d7330fffb7d6a6519b6380
Marco SIlvestri <ItsMarcoSilvestri@gmail.com>
2023-05-14 13:41:09 +0200

    Creation of general scene management

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+public class SceneChange : MonoBehaviour
+{
+
+    private int iMapChoice;
+
+    void Start()
+    {
+
+        iMapChoice = 1;
+
+    }
+
+    void Update()
+    {
+
+
+
+    }
+
+    public void SceneChangeSplash()
+    {
+
+        SceneManager.LoadScene("Splash Scene");
+
+        Debug.Log("Chaning to the 'SPLASH SCENE'");
+
+    }
+
+    public void SceneChangeStart()
+    {
+
+        SceneManager.LoadScene("Start Scene");
+
+        Debug.Log("Chaning to the 'START SCENE'");
+
+    }
+
+    public void SceneChangeOptions()
+    {
+
+        SceneManager.LoadScene("Start Scene");
+
+        Debug.Log("Chaning to the 'OPTIONS SCENE'");
+
+    }
+
+    public void SceneChangeSingle()
+    {
+
+        iMapChoice = Random.Range(1, 3);
+
+        Debug.Log("The chosen map number is " + iMapChoice);
+
+        SceneManager.LoadScene("Single Scene");
+
+        Debug.Log("Chaning to the 'SINGLE SCENE'");
+
+    }
+
+    public void SceneChangeMulti()
+    {
+
+        iMapChoice = Random.Range(1, 3);
+
+        Debug.Log("The chosen map number is " + iMapChoice);
+
+        SceneManager.LoadScene("Multiplayer Scene");
+
+        Debug.Log("Chaning to the 'MULTI SCENE'");
+
+    }
+
+    public void Quit()
+    {
+
+        Application.Quit();
+
+        Debug.Log("The game should 'QUIT'");
+
+    }
+
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour 
+{
+
+	public static string sPlayers;
+	public GameObject gInput;
+
+	void Start () 
+	{
+	
+		
+
+	}
+	
+	void Update () 
+	{
+	
+		
+
+	}
+
+	public void StorePlayers()
+    {
+
+		sPlayers = gInput.GetComponent<Text>().text;
+
+	}
+
+}
